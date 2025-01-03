1f3031d17b7e81cfc4098ce6ae0371a53fa56de8
Junnnnnnnnnnn <wjsguswls71@naver.com>
2025-01-03 14:14:30 +0900

    fix: eslint, prettier conflict

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: 'tsconfig.json',
+    project: "tsconfig.json",
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
+    sourceType: "module",
   },
   plugins: [
-    '@typescript-eslint',
-    'prettier',
-    'import',
-    'unused-imports',
-    'simple-import-sort',
+    "@typescript-eslint",
+    "prettier",
+    "import",
+    "unused-imports",
+    "simple-import-sort",
   ],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:prettier/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "prettier",
   ],
   root: true,
   env: {
@@ -26,66 +27,56 @@ module.exports = {
     jest: true,
     browser: true,
   },
-  ignorePatterns: ['node_modules/', 'dist/', '.eslintrc.js'],
+  ignorePatterns: ["node_modules/", "dist/", ".eslintrc.js"],
   rules: {
-    // Prettier integration
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        trailingComma: 'all',
-        printWidth: 80,
-        tabWidth: 2,
-        endOfLine: 'auto',
-      },
-    ],
-
     // TypeScript rules
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'error',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-inferrable-types': 'off',
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "error",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-base-to-string": "off",
 
     // Import rules
-    'import/order': [
-      'error',
+    "import/order": [
+      "error",
       {
         groups: [
-          ['builtin', 'external'],
-          ['internal', 'parent', 'sibling', 'index'],
+          ["builtin", "external"],
+          ["internal", "parent", "sibling", "index"],
         ],
-        'newlines-between': 'always',
-        alphabetize: { order: 'asc', caseInsensitive: true },
+        "newlines-between": "always",
+        alphabetize: { order: "asc", caseInsensitive: true },
       },
     ],
-    'import/no-unresolved': 'error',
-    'import/no-cycle': 'warn',
-    'import/newline-after-import': 'error',
+    "import/no-unresolved": "error",
+    "import/no-cycle": "warn",
+    "import/newline-after-import": "error",
 
     // Unused imports
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
       {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
       },
     ],
 
     // Simple import sort
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
 
     // General JavaScript rules
-    'no-console': ['warn', { allow: ['warn', 'error'] }],
-    'no-debugger': 'error',
-    'arrow-body-style': ['error', 'as-needed'],
-    'prefer-const': 'error',
-    'no-var': 'error',
-    'object-shorthand': 'error',
-    'no-multiple-empty-lines': ['error', { max: 1 }],
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "no-debugger": "error",
+    "arrow-body-style": ["error", "as-needed"],
+    "prefer-const": "error",
+    "no-var": "error",
+    "object-shorthand": "error",
+    "no-multiple-empty-lines": ["error", { max: 1 }],
   },
 };
